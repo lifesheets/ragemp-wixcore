d46ee559b15de2ed9fad8370f3cccd4bb6242bcf
Mykola Dovhopol <97382411+lifesheets@users.noreply.github.com>
2023-02-09 19:25:06 +0200

    [Add] Команда «Телепортує гравця»

--- packages/commands/teleport.js
+++ packages/commands/teleport.js
@@ -7,3 +7,17 @@ mp.events.addCommand('tpc', (player, _, x, y, z) => {
     }
     player.position = new mp.Vector3(parseFloat(x), parseFloat(y), parseFloat(z));
 })
+
+// Телепортує гравця
+mp.events.addCommand('metp', (player, _, id) => {
+    if (id == undefined) {
+        return player.outputChatBox('/metp [id]');
+    }
+    let target = mp.players.at(id);
+    if (target == null) {
+        return player.notify('~r~ID игрока не найден!');
+    }
+    target.dimension = player.dimension;
+    target.position = player.position;
+    player.notify(`~g~ Вы телепортировали ~y~${target.id} ~g~ID`);
+})
